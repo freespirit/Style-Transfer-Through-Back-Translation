e5fa96f5a694030ccfa57aae971797f1592e5e24
stan0 <freespirit@users.noreply.github.com>
2019-06-04 09:38:17 +0300

    Update the preprocessing example in classifier's example.sh
    According to the README file and all the other examples label0 is republican
    and label1 is democratic. The example for the preprocessing had the values
    reversed.

--- classifier/example.sh
+++ classifier/example.sh
@@ -3,7 +3,7 @@
 ## Note: First create a dev.txt file such that
 ## 1. Each instance is on a new line
 ## 2. The label and the sentence are separated by a space
-python preprocess.py -train_src ../data/political_data/classtrain.txt -label0 democratic -label1 republican -valid_src ../data/political_data/dev.txt -save_data political -src_vocab_size 20000
+python preprocess.py -train_src ../data/political_data/classtrain.txt -label0 republican -label1 democratic -valid_src ../data/political_data/dev.txt -save_data political -src_vocab_size 20000
 
 # Train the classifier
 python cnn_train.py -data political.train.pt -save_model political_model
